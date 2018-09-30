feefb48357d3be60064b9c333049b6c0bdaa98ca
masonlr <masonlr@unimelb.edu.au>
2018-09-30 11:01:01 +0100

    test: Debug middleware connection

--- simulate/status.py
+++ simulate/status.py
@@ -13,9 +13,13 @@ headers = {
 
 def update_status(status: str):
     res = requests.patch(STATUS_URL, json={"status": status}, headers=headers)
+
+    print("DEBUG: CONFIG", CONFIG)
+    print("DEBUG: STATUS_URL", STATUS_URL)
+
     if res.status_code == 200:
         print(f"INFO: Updated job status to {status}.")
-        print(f"INFO: Response json: {res.json()}")
+        print(f"DEBUG: Response json: {res.json()}")
 
     if status == "FINALIZING":
         data = res.json().get("data")
